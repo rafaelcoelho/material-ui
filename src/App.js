5f59b8c3de78f71ca04654c9ced0d7f4ab007e1f
Rafael Coelho <rafael.asilvacoelho@gmail.com>
2022-01-31 17:04:03 -0300

    Add material element with formatting

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import React, { Component, Fragment } from "react";
 import RegisterForm from './components/RegisterForm/registerForm';
+import { Container, Typography } from '@material-ui/core';
+import 'fontsource-roboto'
 
 function App() {
   return (
-    <Fragment>
-      <h1>Register Form</h1>
-      <RegisterForm/>
-    </Fragment>
+    <Container component={'article'} maxWidth='sm'>
+      <Typography variant='h3' component={'h1'} align='center' >Register Form</Typography>
+      <RegisterForm />
+    </Container>
   );
 }
 
--- src/components/RegisterForm/registerForm.jsx
+++ src/components/RegisterForm/registerForm.jsx
@@ -1,29 +1,48 @@
-import React from 'react';
-import Button from "@material-ui/core/Button"
+import React from "react";
+import { TextField, Switch, Button, FormControlLabel } from "@material-ui/core";
 
 function RegisterForm() {
-  return ( 
+  return (
     <form>
-      <label>Name</label>
-      <input type={"text"}/>
+      <TextField
+        id="Name"
+        label="Name"
+        autoComplete="true"
+        variant="outlined"
+        required="true"
+        margin="normal"
+        fullWidth
+      />
+      <TextField
+        id="Family Name"
+        label="Family Name"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+      <TextField
+        id="CPF"
+        label="CPF"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
 
-      <label>Family Name</label>
-      <input type={"text"}/>
+      <FormControlLabel
+        label="Offers"
+        control={<Switch name="Offers" defaultChecked color="primary" />}
+      />
 
-      <label>CPF</label>
-      <input type={"text"}/>
+      <FormControlLabel
+        label="News"
+        control={<Switch name="News" defaultChecked color="primary" />}
+      />
 
-      <label>Offers</label>
-      <input type={"checkbox"}/>
-
-      <label>News</label>
-      <input type={"checkbox"}/>
-
-      <Button typeof='submit' variant="contained" color="primary">
+      <Button typeof="submit" variant="contained" color="primary">
         Register
       </Button>
     </form>
-   );
+  );
 }
 
 export default RegisterForm;
